832f41c4c05ed7b894945c9d38d9f153c4a00d81
pabra <patrick+github-commit-2021@braune.xyz>
2021-07-18 23:44:06 +0200

    wip: union tests

--- test/union.test.ts
+++ test/union.test.ts
@@ -36,6 +36,103 @@ describe('union', () => {
   })
 })
 
+describe('discriminatedUnion', () => {
+  enum Tag {
+    A = 'a_tag',
+    B = 'b_tag',
+    C = 'c_tag',
+  }
+
+  interface A {
+    tag: Tag.A
+    id: number
+  }
+
+  interface B {
+    tag: Tag.B
+    name: string
+  }
+
+  interface C {
+    tag: Tag.C
+    value?: { a: number }
+  }
+
+  type Union = A | B | C
+
+  const runtypeA = st.record({
+    tag: st.literal(Tag.A),
+    id: st.number(),
+  })
+
+  const runtypeB = st.record({
+    tag: st.literal(Tag.B),
+    name: st.string(),
+  })
+
+  const runtypeC = st.record({
+    tag: st.literal(Tag.C),
+    value: st.optional(st.record({ a: st.number() })),
+  })
+
+  it('should accept records that belong to the discriminatedUnion', () => {
+    const runtypeUnion: st.Runtype<Union> = st.union(
+      runtypeA,
+      runtypeB,
+      runtypeC,
+    )
+
+    expectAcceptValuesPure(runtypeUnion, [
+      { tag: 'a_tag', id: 123 },
+      { tag: Tag.B, name: 'asd' },
+      { tag: 'b_tag', name: 'abc' },
+      { tag: 'c_tag', value: { a: 123 } },
+      { tag: 'c_tag' },
+      { tag: 'c_tag', value: undefined },
+    ])
+  })
+
+  it('should reject records that do not belong to the discriminatedUnion', () => {
+    const runtypeUnion = st.union(runtypeA, runtypeB, runtypeC)
+
+    expectRejectValues(runtypeUnion, [
+      { tag: 'x_tag', id: 123 },
+      { tag: 'b_tag', name: [] },
+      { tag: 3, name: 'abc' },
+      99,
+      NaN,
+      undefined,
+      null,
+      { tag: 'a_tag' },
+      [],
+      'foo',
+    ])
+  })
+
+  it('should reject object property names as tags', () => {
+    const runtypeUnion = st.union(
+      st.record({ key: st.literal('a'), value: st.string() }),
+      st.record({ key: st.literal('b'), value: st.string() }),
+    )
+
+    expectRejectValues(
+      runtypeUnion,
+      // JSON parse bc you cant assign to the __proto__ key, but with json it works
+      objectAttributes.map((a) =>
+        JSON.parse(`{"key": "${a}", "value": "asd"}`),
+      ),
+    )
+
+    // proto hacks
+    expectRejectValues(runtypeUnion, [
+      JSON.parse(`{"__proto__": {"key": "a", "value": "asd"}}`),
+      JSON.parse(
+        `{"__proto__": {"key": "a", "value": "asd"}, "key": "a", "value": "asd"}`,
+      ),
+    ])
+  })
+})
+
 describe('discriminatedUnion with different tag types', () => {
   enum Tag {
     A = 'a_tag',
